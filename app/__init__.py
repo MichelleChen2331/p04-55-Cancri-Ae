bdf9004d5d7be570f7d1d82d53244ecf35c30742
MichelleChen2331 <mchen61@cslab1-7>
2026-04-14 11:46:17 -0400

    created data.py and edited __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@ from flask import session, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home', methods=["GET", "POST"])
 def home():
     return render_template("home.html")
+
+@app.route("/definitions", methods=["GET", "POST"])
+def definitions():
+    
 
 if __name__ == '__main__':
     app.debug = True
---
+++ app/data.py
@@ -0,0 +1,6 @@
+# 55-Cancri-Ae (Michelle Chen, Natalie Kieger, Ethan Cheung, Thamidur Rahman)
+import sqlite3                      # enable control of an sqlite database
+import hashlib                      # for consistent hashes
+import secrets                      # to generate ids
+
+DB_FILE="data.db"
