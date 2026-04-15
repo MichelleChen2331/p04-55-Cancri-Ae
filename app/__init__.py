2d58ba8361133f5533b1354ce0c2f7880a969cac
Michelle Chen <mchen61@stuy.edu>
2026-04-14 21:49:06 -0400

    adding a chart onto html and creating a database

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from flask import session, request, redirect
+import plotly.graph_objects as go
+import plotly.utils #plotly helper
+from data import load_planets
 
 app = Flask(__name__)
 
@@ -9,7 +12,43 @@ def home():
 
 @app.route("/definitions", methods=["GET", "POST"])
 def definitions():
-    
+    return render_template("defintions.html")
+
+def build_chart():
+    df = load_planets()
+
+    yearly = df.groupby("disc_year").size().reset_index(name="count")
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x = yearly["disc_year"],
+        y = yearly["count"],
+        marker=dict(
+            color = yearly["count"],
+            colorscale = "Cerulean",
+            showscale = True,
+            colorbar = dict(title="# Planets")
+        ),
+        hovertemplate = (
+            "<b>Year: %{x}</b><br>"
+            "Planets discovered: %{y}<br>"
+            "<extra></extra>"
+        )
+    ))
+
+    fig.update_layout(
+        title = "Exoplanet Discoveries Per Year"
+        xaxis = dict(title="Year", color="white",
+                     gridcolor = "rgba(255,255,255,0.1)"),
+        yaxis = dict(title="Number of Planets", color="white",
+                     gridcolor = "rgba(255,255,255,0.1)"),
+        paper_bgcolor = "rgba(0,0,0,0)",
+        plot_bgcolor = "rgba(20,20,30,0.8)",
+        font = dict(color="white"),
+        margin = dict(l=60, r=40, t=60, b=60),
+    )
+
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 if __name__ == '__main__':
     app.debug = True
--- app/data.py
+++ app/data.py
@@ -1,6 +1,35 @@
 # 55-Cancri-Ae (Michelle Chen, Natalie Kieger, Ethan Cheung, Thamidur Rahman)
 import sqlite3                      # enable control of an sqlite database
 import hashlib                      # for consistent hashes
-import secrets                      # to generate ids
+import pandas as pd
+import requests
 
 DB_FILE="data.db"
+
+def fetch_and_store():
+    """Call NASA API once and save results to database."""
+
+    sql = """
+        SELECT pl_name, disc_year, discoverymethod
+        FROM pscomppars
+        WHERE disc_year IS NOT NULL
+    """
+    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?" + urlencode({
+        "query": sql,
+        "format": "json"
+         })
+    df = pd.DataFrame(requests.get(url, timeout=15).json())
+
+    conn = sqlite3.connect(DB_PATH)
+    df.too_sql("planets", conn, if_exists="replace", index=False)
+    conn.close()
+    print(f"Stored {len(df)} planets in SQLite.")
+
+def load_planets():
+    conn = sqlite3.connect(DB_PATH)
+    df = pd.read_sql("SELECT * FROM planets", conn)
+    conn.close()
+    return df
+
+if __name__ = "__main__":
+    fetch_and_store()
